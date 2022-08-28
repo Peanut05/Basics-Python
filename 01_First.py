643d94be012fec078a3c2dae18013afb69911c12
krushna Gavali <krushnagawali744@gmail.com>
2022-08-28 11:09:42 -0100

    Create 01_First.py

---
+++ 01_First.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+
+# 01 practice problem no 01 writ programe to print twinckle twincle poem 
+
+
+print('''Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle little star
+How I wonder what you are''')
+
+# 02 import any type of module and use it 
+
+import cv2 
