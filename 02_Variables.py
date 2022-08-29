ce0bfb0982e70d944832d67425e435b4aed2f076
krushna Gavali <krushnagawali744@gmail.com>
2022-08-29 12:04:37 -0100

    Create 02_Variables.py

---
+++ 02_Variables.py
@@ -0,0 +1,58 @@
+# assin variable  using python 
+# learn data types in python
+
+a = 10
+b = 20.88
+c = "Krushna"
+d = 777.888
+e = '''        
+        krushna 
+        is 
+        a 
+        good 
+        Boy '''
+
+f = 8485848476
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+
+'''data types using python
+    1)intiger
+    2)float
+    3)charecter
+    4)boolean
+    5)none '''
+
+g = "Ram" # string
+h = 88 # intiger
+i = 88.90 # folat
+j = True # Boolean values
+k = None # None it's tells that none value
+
+# printing variables
+
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+
+# printing types of variable
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))                                      
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
