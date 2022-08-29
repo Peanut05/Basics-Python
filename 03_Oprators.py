db1b588b7dee298f961e61afdd8b5fa400a07d04
krushna Gavali <krushnagawali744@gmail.com>
2022-08-29 12:05:32 -0100

    Create 03_Oprators.py

---
+++ 03_Oprators.py
@@ -0,0 +1,60 @@
+# opreters in python
+
+# Arithmatic Oprators :-
+# it uses for done a arithmatic oprations like 1)addition 2)subtractio 3)multiplications 4)division
+
+print("Addition of 2+2 is",2+2)
+print("subtractin of 2-2 is",2-2)
+print("Multiplication of 2*2 is",2*2)
+print("Division of 2/2 is",2/2)
+
+# Assignment Oprators :-
+# it used for assign values 1)= 2)+= 3)-= 4)*= 5)/= .......etc.
+
+a = 2
+print(a)
+a += 2
+print(a)
+a -= 2
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+
+# Relational Oprators:-
+# it is used to compair two or more values 
+# comparision oprators is 1)== 2)< 3)<= 4)> 5)>= 6)!= .......etc.
+# it returns boolean values
+# = that is use for assign thats why == use for check value.
+
+b = (5==2)
+print(b)
+c = (5<2)
+print(c)
+d = (5>2)
+print(d)
+e = (5<=2)
+print(e)
+f = (5>=2)
+print(f)
+g = (5!=2)
+print(g)
+
+# Logocal Oprators:-
+# logical oprators is 1)and 2)or 3)not
+# logical oprators use to oprate booleans key
+
+# and ::-->> and oprater satisfied both condition then return true otherwise false.
+
+# or ::-->> or oprator when one condition is satisfied then return true then both
+# are false then or returns false.
+
+# not ::-->> not oprator returns negitive value of a condition.
+
+boolean1 = True
+boolean2 = False
+print("values of boolean1 and boolean2",(boolean1 and boolean2))
+print("values of boolean1 or boolean2",(boolean1 or boolean2))
+print("values of boolean1",(not boolean1))
+print("values of boolean1",(not boolean2))
