f6b51c1564f2a7ee5c06023f10f426ec9b8dc269
krushna Gavali <krushnagawali744@gmail.com>
2022-08-30 11:18:20 -0100

    Create 04_Typecasting.py

---
+++ 04_Typecasting.py
@@ -0,0 +1,16 @@
+# Typecasting :-
+'''Typecasting is used for convert one data type to another when it's possible 
+or define a type of data before the variable 
+there are many functions for typecasting of data
+that is 1) str(32) => "32" it's a string
+        2) int("32") => 32 it's a integer 
+        2) Floata(32) => 32.0 it's a float '''
+
+a = "32"
+print(a,type(a)) # Now it's a string   
+a = int(a)   
+print(a,type(a)) # Now it's a intiger
+a = float(a)
+print(a,type(a)) # Now it's a float
+a = str(a)
+print(a,type(a)) # Now it's also become a string
