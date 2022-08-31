6714afef369983e83653f8b6ec6778b0ebee0cf8
krushna Gavali <krushnagawali744@gmail.com>
2022-08-31 15:52:48 -0100

    Create 10_Problrm5.py

---
+++ 10_Problrm5.py
@@ -0,0 +1,9 @@
+# write a python programe to find out average of two numbers entered by users
+
+a = input("Enter first number : ")
+b = input("Enter second number : ")
+a = int(a)
+b = int(b)
+add = (a+b)
+avg = add/2
+print("Average of two numbers is : ",avg)
