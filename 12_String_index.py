ab9a2de7ba29728725b0c9fc0199eae998ce03b3
krushna Gavali <krushnagawali744@gmail.com>
2022-09-01 06:06:58 -0100

    Create 12_String_index.py

---
+++ 12_String_index.py
@@ -0,0 +1,25 @@
+# Index of string :-
+# you can print a single element in a string
+# index is starts from 0 to -1
+string = "Krushna"
+print(string[0])
+print(string[1])
+print(string[2])
+print(string[3])
+print(string[4])
+print(string[5])
+print(string[6])
+
+'''string[0] = 'M' error will shown that
+('str' object does not support item assignment)
+you cannot changes item in between strings '''
+
+# Negitive Index :-
+# it's also use like indexing but it's is starts from -1 -2...
+# it's counting starts ending to begining of the string
+# -1 is shown a [length -1] -2 is shown as [length -2]
+
+a = string[-1:-4]
+print(a)
+print(string[-3])
+print(string[-5])
