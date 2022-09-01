464933d738ce8bae94110ad5072f031282623356
krushna Gavali <krushnagawali744@gmail.com>
2022-09-01 06:18:00 -0100

    Create 15_Escape_sequence_char.py

---
+++ 15_Escape_sequence_char.py
@@ -0,0 +1,15 @@
+# Escape sequence charecter:------>
+# That is the charecter that wiritten after back slash '\'
+'''Escape sequence charecter comprise of more than one charecter
+but represent one charecter when used within the strings'''
+
+''' There are some of escape charecter 
+1)\n  --> That is used for new line
+2)\t  --> That is used for tab
+3)\'  --> That is used for single quote
+4)\\  --> That is used to print backslash'''
+
+print("Krushna is a good boy.\nBut he is lezy also")
+print("Krushna is a good boy.\n\tBut he is lezy also")
+print("\'Krushna\' is a good boy.\'But\' he is \'lezy\' also")
+print("Krushna is a good boy.\\But he is lezy also")
