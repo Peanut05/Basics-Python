d45722c4ee691645ee10e387dc5487ce01a440e1
krushna Gavali <krushnagawali744@gmail.com>
2022-09-02 12:47:17 -0100

    Create 20_Problem.py

---
+++ 20_Problem.py
@@ -0,0 +1,6 @@
+''' Write a program to format the following letter 
+using escape sequence charectres
+hello Krushna
+this is the last problem of this topic
+Thank you! '''
+print("hello Krushna \n\tthis is the last problem of this topic \n\tThank you!") 
