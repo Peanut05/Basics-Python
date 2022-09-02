6e6e9dcd50c37ef6ef3e42b1aa454959eae3772d
krushna Gavali <krushnagawali744@gmail.com>
2022-09-02 12:44:33 -0100

    Create 16_Problem.py

---
+++ 16_Problem.py
@@ -0,0 +1,7 @@
+'''Write a program to display user entered name Followed by Good Morning 
+using input function'''
+
+a = input("Enter Your name :")
+print("Good Morning ,",a)
+# or 
+print("Good Mroning ," + a)
