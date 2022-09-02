5a39851e1838d2139f3ef11dd7fd10ac8b772e86
krushna Gavali <krushnagawali744@gmail.com>
2022-09-02 12:46:34 -0100

    Create 19_Problem.py

---
+++ 19_Problem.py
@@ -0,0 +1,8 @@
+# replace the double spaces from singal spaces in string
+string = "What  are  you  Doing  ?"
+a = string.replace("  " , " ")
+print(a)
+b = "a    b"
+c = b.replace("  " , " ")
+print(c)
+
