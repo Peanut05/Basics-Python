bc5a6727c891d1d2d36fa927a7326d176a469bab
krushna Gavali <krushnagawali744@gmail.com>
2022-09-05 12:30:30 -0100

    Create 22_List_slicing.py

---
+++ 22_List_slicing.py
@@ -0,0 +1,8 @@
+# create list with diffrent data types
+
+numbers = ['one' , 'two' , 3 , 4 , 5.0 , 6.0]
+print(numbers)
+# List sclicing  :-->
+# also can do the sclicing of list like strings
+print(numbers[0:3])
+print(numbers[3:5])
