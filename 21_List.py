8513d9f7d7be994d020f7fb60adc8500d6d01933
krushna Gavali <krushnagawali744@gmail.com>
2022-09-05 12:29:54 -0100

    Create 21_List.py

---
+++ 21_List.py
@@ -0,0 +1,21 @@
+# list :--->
+# Python lists are containers to store a set of values of any data type
+# you can create lists using []
+# ex.-- numbers = [one , two , 3 , 4 , 5.0 , 6.0]
+# There are not issu to repeat values in list
+
+numbers = ['one' , 'two' , 3 , 4 , 5.0 , 6.0]
+print(numbers)
+
+# Indexing :-->
+# list can accessing by indexing of lists
+
+print(numbers[0])
+print(numbers[5])
+
+# change value in lists
+list1 = ['s','a','u']
+print(list1)
+list1[2] = 'i'
+print(list1)
+
