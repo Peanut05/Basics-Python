03868745c491a1d386367c2b3df6751156b71de9
krushna Gavali <krushnagawali744@gmail.com>
2022-09-07 14:50:41 -0100

    Create 26_Problem.py

---
+++ 26_Problem.py
@@ -0,0 +1,14 @@
+'''Write a programe to store a eight names in a list
+ entered by the user'''
+
+n1 = input("Enter name 1 :")
+n2 = input("Enter name 2 :")
+n3 = input("Enter name 3 :")
+n4 = input("Enter name 4 :")
+n5 = input("Enter name 5 :")
+n6 = input("Enter name 6 :")
+n7 = input("Enter name 7 :")
+n8 = input("Enter name 8 :")
+
+name_list = [n1, n2, n3, n4, n5, n6, n7, n8]
+print("Names list is :\n",name_list)
