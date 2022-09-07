e8e1d4d62a93d443b91fcc530f036c43f775078b
krushna Gavali <krushnagawali744@gmail.com>
2022-09-07 14:53:30 -0100

    Create 30_Problem.py

---
+++ 30_Problem.py
@@ -0,0 +1,7 @@
+''' Write a programe to count a number of zeros in the 
+following tuple
+tpl = (1,0,44,55,0,8,9,0,0,21,0)'''
+
+tpl = (1,0,44,55,0,8,9,0,0,21,0)
+a = tpl.count(0)
+print("Zeros in following tuple is : ",a)
