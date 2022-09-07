e7d9e8681e1f6002aa283b62b56e7dac3f4b40c7
krushna Gavali <krushnagawali744@gmail.com>
2022-09-07 14:49:10 -0100

    Create 24_Tuples.py

---
+++ 24_Tuples.py
@@ -0,0 +1,13 @@
+# Tuple :--->
+# Tuple is a immutable [cannot change] data type in python 
+# Creating a tupels using () 
+# Tupeles value cannot update like lists
+# It's also get access by indexing
+''' Empty tuple ()
+tuple with one element(1,) in tuples only one element need comma  after assign value
+tuple with more than one element(1,2,3,5)'''
+
+tuple1 = (1 , 2 , 3 , 4 , 5 )
+print(tuple1)
+print(tuple1[2]) # access by indexing
+print(tuple1[0:5]) # sclicing 
