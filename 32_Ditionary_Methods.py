82280bea9d85742acc803bd29d11f8c9fd84e44d
krushna Gavali <krushnagawali744@gmail.com>
2022-09-08 12:11:25 -0100

    Create 32_Ditionary_Methods.py

---
+++ 32_Ditionary_Methods.py
@@ -0,0 +1,37 @@
+# Dtionary methods :--->
+# that method used to do some oprations on ditionary
+
+dicto1 = {
+            "karan": "arjun",
+            "champaklal": "jethalal",
+            "popatlal": "umbrela",
+            1: 3232,
+            2: 4343
+         }
+# 1) ditionary.keys() :--
+# It print overall keys that contains in the entire list. 
+
+# 2) ditionary.values() :--
+# It prints overall values that contained ditionary.
+
+# 3) ditionary.items() :--
+# it prints all items that is keys and values in ditionary.
+
+# 4) dictionary.update() :--
+# that function update key and values in ditionary
+
+# 5) ditonary.get()--->
+# that also prints the value of an element (but when its not presents in list
+# it prints none)
+
+# print(dicto1.keys()) # print keys in sictionary
+#print(dicto1.values()) # print values in ditionary
+#print(dicto1.items()) # print overall contents in ditionary
+#print(type(dicto1.keys()) # print type of data
+print(dicto1.get("karan"))
+
+# print(list(dicto1.keys())) # change ditionary to list [typecasting]
+
+
+
+
