b3faa43c8e82393997edc7d8de40c95c3bdf6d62
krushna Gavali <krushnagawali744@gmail.com>
2022-09-08 12:13:01 -0100

    Create 33_Sets.py

---
+++ 33_Sets.py
@@ -0,0 +1,17 @@
+# Sets in python : --->
+# sets is a collection of non repeatative elements in python
+# syntsx of set is ---
+# a = {1,2,3}
+
+# properties of set :----->
+# 1) sets are unordered (element's order dosen't matter/)
+# 2) set are unindexed (can't access set by element's index)
+# 3) There is no way to changes elements in sets
+# 4) Set cannot contain duplicate values.
+
+
+a = {1,2,3,4,2}
+print(a)
+print(type(a)) 
+
+
