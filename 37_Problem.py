08d174a7b629ee86f0f7d4aa8662e4c7fe1bba7c
krushna Gavali <krushnagawali744@gmail.com>
2022-09-09 14:59:54 -0100

    Create 37_Problem.py

---
+++ 37_Problem.py
@@ -0,0 +1,14 @@
+''' Write a programe to input eight numbers from the user and 
+display all the unique numbers [once] '''
+
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+n3 = int(input("Enter number 3 :"))
+n4 = int(input("Enter number 4 :"))
+n5 = int(input("Enter number 5 :"))
+n6 = int(input("Enter number 6 :"))
+n7 = int(input("Enter number 7 :"))
+n8 = int(input("Enter number 8 :"))
+
+s1 = {n1,n2,n3,n4,n5,n6,n7,n8}
+print(s1)
