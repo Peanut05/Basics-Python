5aae64fe9b126a442d4d661fffc40df4cb5697e8
krushna Gavali <krushnagawali744@gmail.com>
2022-09-10 11:11:26 -0100

    Create 42_Problem.py

---
+++ 42_Problem.py
@@ -0,0 +1,8 @@
+''' Can you change the value inside the list 
+which is command in a set s1 
+s1 = {1,2,3,"ABCD",[4,5,6]}
+'''
+
+s1 = {1,2,3,"ABCD",[4,5,6]} 
+
+# error will accurd cause set is unhashable and list is hashable
