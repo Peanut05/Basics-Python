c0513c01265a933373eb7c31acfb97179e9d31b1
krushna Gavali <krushnagawali744@gmail.com>
2022-09-11 08:23:28 -0100

    Create 46_is_and_in.py

---
+++ 46_is_and_in.py
@@ -0,0 +1,19 @@
+# is :---------------------------------->>
+# is work like is that is value is equal or not.
+
+a = None
+
+if (a is None): # check a == none.
+    print("a is none \n")
+else:
+    print("a is not none \n")
+
+# in :----------------------------------->>
+# it retuns boolean value
+# in check the value in it or not.
+# syntax .- value in variable_name
+
+a = [12,11,13,14,15]
+
+print(11 in a) # 11 is in list a thats why pirnt true
+print(111 in a) # 111 is not in list a thats why prnt false
