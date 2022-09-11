071850d7136d016c1729e4268c944ca8e228f567
krushna Gavali <krushnagawali744@gmail.com>
2022-09-11 08:22:02 -0100

    Create 44_Practice_Question.py

---
+++ 44_Practice_Question.py
@@ -0,0 +1,11 @@
+''' Write a programe to print yes when the age entered by user 
+is greter than 18 or equal to 18 or print no '''
+
+age = int(input("Enter your age : \n"))
+
+if(age>18):
+    print("Yes!")
+else:
+    print("No!")
+
+# Remember oprators
