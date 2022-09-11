f9c49f5159f036c38e12be4317a64b63f96a2890
krushna Gavali <krushnagawali744@gmail.com>
2022-09-11 08:22:27 -0100

    Create 45_Practice_Question.py

---
+++ 45_Practice_Question.py
@@ -0,0 +1,8 @@
+''' Write a programe you are of our age group or not
+that is 20 to 38 '''
+
+age = int(input("Enter your age : "))
+if(age>20 and age<=38):
+    print("You are of our age group !\n")
+else:
+    print("You are not of our age group !\n")
