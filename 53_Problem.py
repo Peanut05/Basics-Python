4b910e2f0515294220608b994f28c02f8dbb4f04
krushna Gavali <krushnagawali744@gmail.com>
2022-09-13 11:22:33 -0100

    Create 53_Problem.py

---
+++ 53_Problem.py
@@ -0,0 +1,10 @@
+# Write a programe to find out a given post is talking about "python" or not .
+
+post = input("Enter your post :")
+
+if(post in "python"):
+    print("The post is about python")
+else( post in "PYTHON"):
+    print("The post is about python")
+else:
+    print("The post is not about python")
