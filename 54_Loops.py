617fafb8a42fa9de34c5206b0d9a4d9bae7e19fb
krushna Gavali <krushnagawali744@gmail.com>
2022-09-13 11:23:15 -0100

    Create 54_Loops.py

---
+++ 54_Loops.py
@@ -0,0 +1,27 @@
+# Loops in python :-------------->>
+''' Sometimes we want to repeat a set of statements or set of instructions
+    in a programe for instance then use loop 
+    example : print number 1 to 1000 '''
+
+''' Loops make it easy for a programer to tell the computer
+    which set of instructions to repeat and how '''
+
+''' In a while loops the condition is checked out first.
+    if it's evalueates true , the body of the loop is executed , otherwise not
+    
+    If the loop is entered the process of loop[conditon check and execution]
+    is continued until the condition becomes false '''
+
+# Types of loop in python :---------------------->>
+# 1) While loop ==>>
+
+''' syntax ==>>     while condition: ...[**the block repeats until the conditon is true]
+                            # Body of the loop '''
+
+# 2) For loop ==>>
+
+''' For loop is used to itarate through a sequence be like 
+    list , tuple , or string '''
+
+''' Syntax ==>>     for condition:
+                            # Body of the loop '''
