ad25ffbb0b8c3a4448774c38ba5b28a53ef69f0f
krushna Gavali <krushnagawali744@gmail.com>
2022-09-13 11:20:54 -0100

    Create 51_Problem.py

---
+++ 51_Problem.py
@@ -0,0 +1,11 @@
+''' Write a programe which find out a given name is
+    present even in the list or not . '''
+
+l1 = ["prasad" , "sakshi" , "kumawat" , "patil"]
+
+a = input("Enter a name that is present in the list or not : \n")
+
+if(a in l1):
+    print("Name is present in the list ")
+else:
+    print("Name is not present in the list")
