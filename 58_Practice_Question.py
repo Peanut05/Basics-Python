a704866a249c8fcac6017e3246fc0a5124e3aff5
krushna Gavali <krushnagawali744@gmail.com>
2022-09-14 09:11:23 -0100

    Create 58_Practice_Question.py

---
+++ 58_Practice_Question.py
@@ -0,0 +1,9 @@
+# Write a programe to print the content of a list using while loop
+
+fruits = ["orange","banana","papaya","mango","apple"]
+
+f = 0
+while f < len(fruits):
+        print(fruits[f])
+        f = f + 1
+ 
