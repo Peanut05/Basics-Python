c23844614ea9f596bce74430581151638e102f4e
krushna Gavali <krushnagawali744@gmail.com>
2022-09-14 09:08:15 -0100

    Create 55_While_Loop.py

---
+++ 55_While_Loop.py
@@ -0,0 +1,11 @@
+# While loop in python :--------->
+
+''' syntax ==>>     while condition: ...[**the block repeats until the conditon is true]
+                            # Body of the loop '''
+
+
+i = 0
+while i<11 :    
+    print("Yes! " + str(i))
+    i = i + 1
+print("Done! ")
