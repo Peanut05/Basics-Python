18521b8559d68ee6e5e500e66373574aee9796fb
krushna Gavali <krushnagawali744@gmail.com>
2022-09-15 09:27:52 -0100

    Create 62_For_Loop_With_Else.py

---
+++ 62_For_Loop_With_Else.py
@@ -0,0 +1,11 @@
+# for with else :
+#  an optional else can be used with the for loop  
+# and after for loop exits (executes completely) from the loop the else part is executed 
+
+for i in range(10):
+    print(i)
+
+else:
+    print("Else part will execute after exit from for")
+
+# we can use else with while loop as similler as for 
