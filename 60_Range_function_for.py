466bb0dd975efea4a28bd46c11913eb533dbc8b8
krushna Gavali <krushnagawali744@gmail.com>
2022-09-15 09:25:45 -0100

    Create 60_Range_function_for.py

---
+++ 60_Range_function_for.py
@@ -0,0 +1,19 @@
+# Range Function in python :==>>
+
+''' The range function is used to generate
+    a sequence of numbers''' 
+
+   
+for i in range(9): # By default start from 0
+    print(i) #it prints value from 0 to n-1
+
+for i in range(2 , 9) :  # Starts from 2
+    print(i)
+
+
+'''We can also specify start , stop and step
+    size as follows in range function
+    syntax :==>> range(start,stop,step-size)  '''
+
+for i in range(0 , 10 , 2): # By default step-sze is one.
+    print(i)  # step size is not usually use with range functio.
