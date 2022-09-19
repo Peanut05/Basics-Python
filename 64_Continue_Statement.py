786aab4761b5da2edd2f90db91297806a43861c3
krushna Gavali <krushnagawali744@gmail.com>
2022-09-19 08:57:43 -0100

    Create 64_Continue_Statement.py

---
+++ 64_Continue_Statement.py
@@ -0,0 +1,9 @@
+# continue Statement ::==>>
+''' Continue statement is used to stop the current iteration of the loop 
+and continue with the next one it's instructs the programe to skip this 
+iteration ex______'''
+
+for i in range(11):
+    if i == 5:  #it skip this step and continue to next condition
+        continue
+    print(i)
