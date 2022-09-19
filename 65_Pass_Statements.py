dc35fd42e754813677d57f623e98a204dbda5a9c
krushna Gavali <krushnagawali744@gmail.com>
2022-09-19 08:58:26 -0100

    Create 65_Pass_Statements.py

---
+++ 65_Pass_Statements.py
@@ -0,0 +1,17 @@
+# Pass Statement : ----->
+''' Pass Statement is a null statement in python
+    It's instructs "to nouthing" '''
+
+i = 5
+if i>0 : #emoty if else statement 
+    pass 
+else:
+    pass
+
+while i<0: #empty while loop
+    pass
+
+def funkk (a ,b):    #empty function
+    pass
+
+print("This is pass function in python")
