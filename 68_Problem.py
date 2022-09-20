94c117615e1da5c19fc9a8eea30ca84be839132a
krushna Gavali <krushnagawali744@gmail.com>
2022-09-20 14:07:03 -0100

    Create 68_Problem.py

---
+++ 68_Problem.py
@@ -0,0 +1,9 @@
+''' Write a programe to greet all persons name in 
+    stored in a list L1 and find out which name starts with 's' '''
+
+L1 = ["rahul" , "karan" , "shaym" , "saurav" , "rohan"]
+for name in L1:
+    if name.startswith("s"): #starstwith find string starts with or not
+    
+        print ("Hello "+ name)
+
