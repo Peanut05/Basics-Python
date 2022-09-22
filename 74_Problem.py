0ede1525f83aec34ca14dd8796dacf0908c03763
krushna Gavali <krushnagawali744@gmail.com>
2022-09-22 09:31:56 -0100

    Create 74_Problem.py

---
+++ 74_Problem.py
@@ -0,0 +1,13 @@
+'''write a program to print following star pattern 
+    ***
+    * *
+    ***         '''
+
+n = 3
+for i in range(3):
+    print("*")
+    print(" ")
+    print("*")    
+    
+   
+  #Problem inside
