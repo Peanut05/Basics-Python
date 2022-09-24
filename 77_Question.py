e057e8d240d3f52747da30f3a2233c67e4a4a14c
krushna Gavali <krushnagawali744@gmail.com>
2022-09-24 12:20:37 -0100

    Create 77_Question.py

---
+++ 77_Question.py
@@ -0,0 +1,14 @@
+''' Write a programe to greet a user with good day with the functions '''
+
+# user input:====>>>
+'''def gd():
+    return print("Good Day ! ")
+
+name = input("Enter your name : ")
+print(name)
+gd()'''
+
+# or method:---->
+def greet(name2):
+    print("Good Day ," + name2)
+greet("prasad")
