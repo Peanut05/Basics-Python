38880bc49988408a86e78b661b1f17f0a082b1f9
krushna Gavali <krushnagawali744@gmail.com>
2022-09-26 15:52:07 -0100

    Create 82_Problem.py

---
+++ 82_Problem.py
@@ -0,0 +1,7 @@
+''' How do you prevent a python print function to 
+    prnt a new line at the end . use end function'''
+
+print("Hello!,",end=" ") #privent in the double code in end that will prevent after print fun
+print("how",end=" ")
+print("are",end=" ")
+print("you?",end=" ")
