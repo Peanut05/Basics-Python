8bb8392602d65ae2919f3ff6d7b43012061b3656
krushna Gavali <krushnagawali744@gmail.com>
2022-09-26 15:54:22 -0100

    Create 84_Problem.py

---
+++ 84_Problem.py
@@ -0,0 +1,11 @@
+''' Write a pythoon function to print the first n lines of 
+    the following pattern 
+
+    ***
+    **          #for n = 3
+    *           '''
+
+n = 3
+for i in range(n):
+    print("*" * (n-i))  #prints * (n-i) times 
+
