ec362bc4bcfc3b81a7b4880e2ea42fe0de5141ad
krushna Gavali <krushnagawali744@gmail.com>
2022-09-26 15:55:04 -0100

    Create 85_Problem.py

---
+++ 85_Problem.py
@@ -0,0 +1,6 @@
+''' Write a python programe to convert inches to cms '''
+
+inch = int(input("Enter inches to change into centimeter"))
+cm = inch * 2.54
+
+print(f"{inch} inches is {cm} Centimeter ")
