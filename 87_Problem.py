7d5f5dc058a8df0f2b8b411492e5d9f430475bf7
krushna Gavali <krushnagawali744@gmail.com>
2022-10-02 22:06:34 +0530

    Create 87_Problem.py

---
+++ 87_Problem.py
@@ -0,0 +1,11 @@
+''' Write a python function to print multiplication 
+    table of a given number '''
+
+def tabl(num1):
+    for i in range(11):
+        return num1 * i
+
+k = 11
+tabl(k)
+print(num1)
+
