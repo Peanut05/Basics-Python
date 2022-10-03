8ca802935d5b85d2efc1174646a332559898d56f
krushna Gavali <krushnagawali744@gmail.com>
2022-10-03 15:51:13 +0530

    Create 89_File_IO.py

---
+++ 89_File_IO.py
@@ -0,0 +1,31 @@
+# File I/O :====>>>
+
+''' The random access memory is volatile and and all it's content
+    are lost once a programe terminates order persist the data forever 
+    we use files '''
+
+''' A file is data stored in a storage device. A python program can
+    talk to the file by reading content form it and writing content to it '''
+
+    #[programer] ---> [comp program writen in python] write>> <<read [File]
+
+''' ram == volatile
+    rom/hdd == non-volatile'''
+
+# Type of files :====>>>
+# There are  two types of file 
+#1) Text files (.txt , .c etc)
+#2) Binary files (.jpg , .dat etc)
+#python has a lot of functions for reaading , updating and deleting files.
+'''
+#Opening Files :===>>>
+Python has an open function for opening files. It takes 2
+parameter : filename and mode .'''
+
+'''
+open ("exmp.txt","r")
+
+In this open function contain two types of parameter that is exmp.txt is a file 
+name and "r" is mode (it means perform opration on the file)  
+
+'''
